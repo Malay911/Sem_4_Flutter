0269652363b0a73f520e1129b109339d82c222e8
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-12-01 22:00:26 +0530

    Delete add.dart

--- add.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-void main(){
-  print("enter number1:");
-  int a=int.parse(stdin.readLineSync()!);
-  print("enter number2:");
-  int b=int.parse(stdin.readLineSync()!);
-  print(a+b);
-}
